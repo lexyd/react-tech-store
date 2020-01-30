6ed64e318810d82d63afa10f16a9011dfd59c3b3
destiny <destiny-mac@destinys-MacBook-Pro.local>
2020-01-30 13:00:24 +0100

    first commint message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from tech sctore</h1>
+      </div>
+    )
+  }
+}
